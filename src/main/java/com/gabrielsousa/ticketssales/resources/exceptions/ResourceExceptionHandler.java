4a7a984b1a64bb7efc15872d68dc7efe836a337e
GabrielSousaJS <gabriels.santosx@gmail.coom>
2022-10-31 11:16:39 -0300

    Exception handling: delete - User

--- src/main/java/com/gabrielsousa/ticketssales/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/gabrielsousa/ticketssales/resources/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.gabrielsousa.ticketssales.resources.exceptions;
 
+import com.gabrielsousa.ticketssales.services.exceptions.DatabaseException;
 import com.gabrielsousa.ticketssales.services.exceptions.ResourceNotFoundException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,6 +18,14 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<StandardError> database(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
